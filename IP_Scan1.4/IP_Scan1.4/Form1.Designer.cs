5426c8d89b7011e564564236d71d5e24316421b2
BlackBeard <monish.ramadoss@outlook.com>
2018-07-21 23:21:15 -0700

    Add files via upload

---
+++ IP_Scan1.4/IP_Scan1.4/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace IP_Scan1._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstLocal = new System.Windows.Forms.ListView();
+            this.StartIP = new System.Windows.Forms.TextBox();
+            this.EndIP = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.MaxPortValue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstLocal
+            // 
+            this.lstLocal.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton;
+            this.lstLocal.Alignment = System.Windows.Forms.ListViewAlignment.SnapToGrid;
+            this.lstLocal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstLocal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstLocal.Location = new System.Drawing.Point(127, 12);
+            this.lstLocal.Name = "lstLocal";
+            this.lstLocal.Size = new System.Drawing.Size(710, 285);
+            this.lstLocal.TabIndex = 0;
+            this.lstLocal.UseCompatibleStateImageBehavior = false;
+            // 
+            // StartIP
+            // 
+            this.StartIP.Location = new System.Drawing.Point(12, 33);
+            this.StartIP.Name = "StartIP";
+            this.StartIP.Size = new System.Drawing.Size(100, 20);
+            this.StartIP.TabIndex = 1;
+            this.StartIP.Text = "10.8.0.1";
+            // 
+            // EndIP
+            // 
+            this.EndIP.Location = new System.Drawing.Point(12, 74);
+            this.EndIP.Name = "EndIP";
+            this.EndIP.Size = new System.Drawing.Size(100, 20);
+            this.EndIP.TabIndex = 2;
+            this.EndIP.Text = "10.8.1.0";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(12, 274);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 5;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Start IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "End IP";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(127, 303);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(710, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MaxPortValue
+            // 
+            this.MaxPortValue.Location = new System.Drawing.Point(12, 214);
+            this.MaxPortValue.Name = "MaxPortValue";
+            this.MaxPortValue.Size = new System.Drawing.Size(100, 20);
+            this.MaxPortValue.TabIndex = 10;
+            this.MaxPortValue.Text = "65535";
+            // 
+            // label4
+            // 
+            this.label4.AutoEllipsis = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Max Port Value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 345);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MaxPortValue);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.EndIP);
+            this.Controls.Add(this.StartIP);
+            this.Controls.Add(this.lstLocal);
+            this.Name = "Form1";
+            this.Text = "IPSCAN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstLocal;
+        private System.Windows.Forms.TextBox StartIP;
+        private System.Windows.Forms.TextBox EndIP;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox MaxPortValue;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ IP_Scan1.4/IP_Scan1.4/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+struct Variables
+{
+
+    public static int counter = 0;
+    public static IPAddress startIP;
+    public static IPAddress endIP;
+    public static List<IPAddress> listofIP = new List<IPAddress>();
+    public static List<ListViewItem> listofeverythin = new List<ListViewItem>();
+    public static Thread MainThread;
+
+};
+
+
+
+namespace IP_Scan1._4
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       /// <summary>
+       /// main Ping function
+       /// </summary>
+
+        public void Ping_all()
+        {
+            foreach (IPAddress i in Variables.listofIP)
+            {
+                Ping(i.ToString(), 4, 2000);
+            }
+
+        }
+        /// <summary>
+        /// Gives list of IP to a public list
+        /// </summary>
+        /// 
+        public void giveIP()
+        {
+            Variables.startIP = IPAddress.Parse(StartIP.Text);
+            Variables.endIP = IPAddress.Parse(EndIP.Text);
+            
+            List<IPAddress> lofIP = new List<IPAddress>();
+            var bytes1 = Variables.startIP.GetAddressBytes();
+            var bytes2 = Variables.endIP.GetAddressBytes();
+            Array.Reverse(bytes1);
+            Array.Reverse(bytes2);
+
+            uint tempval_1 = BitConverter.ToUInt32(bytes1, 0);
+            uint tempval_2 = BitConverter.ToUInt32(bytes2, 0);
+
+            for (uint i = tempval_1; i < tempval_2; i++)
+            {
+                if (lofIP.Count != tempval_2 - tempval_1)
+                {
+                    var bytes = BitConverter.GetBytes(i);
+                    Array.Reverse(bytes);
+                    lofIP.Add(new IPAddress(bytes));
+                }
+            }
+            Variables.listofIP = lofIP;
+        }
+
+        /// <summary>
+        /// Ping
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="att"></param>
+        /// <param name="time"></param>
+        public void Ping(string host, int att, int time)
+        {
+            
+            for(int i = 0; i < att; i++)
+            {
+                bool caught = false;
+
+                new Thread(delegate ()
+                {
+                    try
+                    {
+                        System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+                        ping.PingCompleted += new PingCompletedEventHandler(PingCompleted);
+                        ping.SendAsync(host, time, host);
+                       
+                    }
+                    catch
+                    {
+                        caught = true;
+                        Variables.counter++;
+                    }
+                    if (!caught)
+                    {
+                        Thread.CurrentThread.Abort();
+                    }
+                }).Start();
+            }
+        }
+
+        /// <summary>
+        /// Ping handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void PingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            
+            string ip = (string)e.UserState;
+            if (e.Reply != null && e.Reply.Status == IPStatus.Success)
+            {
+                string hostname = NetInfo.GetHostName(ip);
+                string macaddres = NetInfo.GetMacAddress(ip);
+                string ports = " ";
+                List<int> portisopen = NetInfo.getportid(ip,Convert.ToInt32(MaxPortValue.Text));
+                string[] arr = new string[4];
+                
+                foreach (int i in portisopen){
+                    ports = ports + "," + i.ToString();
+                }
+                //store all three parameters to be shown on ListView
+                arr[0] = ip;
+                arr[1] = hostname;
+                arr[2] = macaddres;
+                arr[3] = ports;
+
+                // Logic for Ping Reply Success
+                ListViewItem item;
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        item = new ListViewItem(arr);
+
+                        lstLocal.Items.Add(item);
+                        Variables.counter++;
+
+                    }));
+                }
+            }
+            else
+            {
+                // MessageBox.Show(e.Reply.Status.ToString());
+            }
+            
+        }
+
+
+        /// <summary>
+        /// Loads form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] array = NetInfo.NetworkGateway().Split('.');
+            giveIP();
+            lstLocal.View = View.Details;
+            lstLocal.Clear();
+            lstLocal.GridLines = true;
+            lstLocal.FullRowSelect = true;
+            lstLocal.BackColor = System.Drawing.Color.Aquamarine;
+            lstLocal.Columns.Add("IP", 100);
+            lstLocal.Columns.Add("HostName", 200);
+            lstLocal.Columns.Add("MAC Address", 300);
+            lstLocal.Columns.Add("PORTS",400);
+            lstLocal.Sorting = SortOrder.Descending;
+            StartIP.Text = array[0] + "." + array[1] + "." + array[2] + "." + array[3];
+            EndIP.Text = array[0] + "." + array[1] + "." + array[2] + "." + "255";
+            progressBar1.Maximum = Variables.listofIP.Count;
+            timer1.Start();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            giveIP();
+            Variables.MainThread = new Thread(delegate ()
+            {
+                Ping_all();
+            });
+            Variables.MainThread.Name = "MAINTHREAD";
+            Variables.MainThread.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = Variables.counter;
+            timer1.Interval += 1;
+        }
+    }
+}
